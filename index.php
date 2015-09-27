f8bdd8ab8a9c0d0e8ad4a8b6af3a1f9b537a43cc
Alexandr <maler1988@yandex.ru>
2015-09-27 22:50:53 +0300

    Вёрстка списка купонов

--- index.php
+++ index.php
@@ -3,43 +3,47 @@
 	<head>
 		<title>Умные покупки - промокоды</title>
 		<meta charset="utf-8" >
+		<link href='https://fonts.googleapis.com/css?family=Noto+Sans:400,400italic,700,700italic&subset=latin,cyrillic-ext' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" href="/css/style.css" >
 	</head>
 	<body>
-	<div class="center-container" >
-		<header>
-			<div class="logo" >
-				<a href="/">
-					<img alt="Умные-покупки.рф" src="/images/logo.png">
-				</a>
-			</div>
-			<div class="top-menu" >
+	<div class="header-container" >
+		<div class="center-container" >
+			<header>
+				<div class="logo" >
+					<a href="/">
+						<img alt="Умные-покупки.рф" src="/images/logo.png">
+					</a>
+				</div>
+				<div class="top-menu" >
+					<ul>
+						<li><a href="#" >Вопросы и ответы</a></li>
+						<li><a href="#" >О проекте</a></li>
+						<li><a href="#" >Лайфхак</a></li>
+						<li><a href="#" >Контакты</a></li>
+					</ul>
+				</div>
+				<div class="search" >
+					<form class="search-form" action="" >
+						<input type="text" name="q" placeholder="Поиск промокодов, магазинов и скидок" >
+						<input type="submit" name="submit" value="Найти" >
+					</form>
+				</div>
+			</header>
+		</div>
+		<div class="center-container" >
+			<div class="middle-menu" >
 				<ul>
-					<li><a href="#" >Вопросы и ответы</a></li>
-					<li><a href="#" >О проекте</a></li>
-					<li><a href="#" >Лайфхак</a></li>
-					<li><a href="#" >Контакты</a></li>
+					<li><a href="#" >Популярные</a></li>
+					<li><a href="#" >Купоны по категориям</a></li>
+					<li><a href="#" >Новые</a></li>
+					<li><a href="#" >Горящие</a></li>
+					<li><a href="#" >Все магазины</a></li>
 				</ul>
 			</div>
-			<div class="search" >
-				<form class="search-form" action="" >
-					<input type="text" name="q" >
-					<input type="submit" name="submit" value="Найти" >
-				</form>
-			</div>
-		</header>
-	</div>
-	<div class="center-container" >
-		<div class="middle-menu" >
-			<ul>
-				<li><a href="#" >Популярные</a></li>
-				<li><a href="#" >Купоны по категориям</a></li>
-				<li><a href="#" >Новые</a></li>
-				<li><a href="#" >Горящие</a></li>
-				<li><a href="#" >Все магазины</a></li>
-			</ul>
 		</div>
 	</div>
+	
 	<div class="center-container" >
 		<div class="content" >
 			<div class="content-area" >
@@ -59,14 +63,20 @@
 								<p>Без ввода промо-кода. Предложение ограничено. 
 								Скидка распространяется только на товары из данного раздела.</p>
 								
-								<a href="#">Использовать промокод</a>
-								<a href="#">все промокоды</a>
+								<a class="use-promo" href="#">Использовать промокод</a>
+								<a class="see-all-coupons" href="#">все промокоды</a>
 							</div>
 							<div  class="promo-item-block-right" >
-								<span>Действителен ещё</span>
-								<span class="coupon-time" >
-									<strong>1</strong> день
-								</span>
+								<div class="promo-item-sale-marker persent" >
+									<span>скидка</span>
+									<strong>50%</strong>
+								</div>
+								<div class="promo-item-coupon-timer" >
+									<span>Действителен ещё</span>
+									<span class="coupon-time" >
+										<strong>1</strong> день
+									</span>
+								</div>
 							</div>
 						</div>
 					</li>
@@ -74,7 +84,7 @@
 					<li class="promo-item">
 						<div class="promo-item-block">
 							<div  class="promo-item-block-left" >
-								<img alt="" src="/images/coupon-image.jpg" class="promo-item-img" >
+								<img alt="" src="/images/coupon-image-2.jpg" class="promo-item-img" >
 								<div class="vote-block" ></div>
 							</div>
 							<div  class="promo-item-block-center" >
@@ -84,14 +94,20 @@
 								<p>Без ввода промо-кода. Предложение ограничено. 
 								Скидка распространяется только на товары из данного раздела.</p>
 								
-								<a href="#">Использовать промокод</a>
-								<a href="#">все промокоды</a>
+								<a class="use-promo" href="#">Использовать промокод</a>
+								<a class="see-all-coupons" href="#">все промокоды</a>
 							</div>
 							<div  class="promo-item-block-right" >
-								<span>Действителен ещё</span>
-								<span class="coupon-time" >
-									<strong>1</strong> день
-								</span>
+								<div class="promo-item-sale-marker free-delivery" >
+									<span>Басплатная<br/> доставка</span>
+									<strong>&nbsp;</strong>
+								</div>
+								<div class="promo-item-coupon-timer" >
+									<span>Действителен ещё</span>
+									<span class="coupon-time" >
+										<strong>1</strong> день
+									</span>
+								</div>
 							</div>
 						</div>
 					</li>
@@ -99,7 +115,7 @@
 					<li class="promo-item">
 						<div class="promo-item-block">
 							<div  class="promo-item-block-left" >
-								<img alt="" src="/images/coupon-image.jpg" class="promo-item-img" >
+								<img alt="" src="/images/coupon-image-3.jpg" class="promo-item-img" >
 								<div class="vote-block" ></div>
 							</div>
 							<div  class="promo-item-block-center" >
@@ -109,14 +125,20 @@
 								<p>Без ввода промо-кода. Предложение ограничено. 
 								Скидка распространяется только на товары из данного раздела.</p>
 								
-								<a href="#">Использовать промокод</a>
-								<a href="#">все промокоды</a>
+								<a class="use-promo" href="#">Использовать промокод</a>
+								<a class="see-all-coupons" href="#">все промокоды</a>
 							</div>
 							<div  class="promo-item-block-right" >
-								<span>Действителен ещё</span>
-								<span class="coupon-time" >
-									<strong>1</strong> день
-								</span>
+								<div class="promo-item-sale-marker cash" >
+									<span>скидка</span>
+									<strong>500 <span>руб.</span></strong>
+								</div>
+								<div class="promo-item-coupon-timer" >
+									<span>Действителен ещё</span>
+									<span class="coupon-time" >
+										<strong>1</strong> день
+									</span>
+								</div>
 							</div>
 						</div>
 					</li>
@@ -134,14 +156,20 @@
 								<p>Без ввода промо-кода. Предложение ограничено. 
 								Скидка распространяется только на товары из данного раздела.</p>
 								
-								<a href="#">Использовать промокод</a>
-								<a href="#">все промокоды</a>
+								<a class="use-promo" href="#">Использовать промокод</a>
+								<a class="see-all-coupons" href="#">все промокоды</a>
 							</div>
 							<div  class="promo-item-block-right" >
-								<span>Действителен ещё</span>
-								<span class="coupon-time" >
-									<strong>1</strong> день
-								</span>
+								<div class="promo-item-sale-marker persent" >
+									<span>скидка</span>
+									<strong>50%</strong>
+								</div>
+								<div class="promo-item-coupon-timer" >
+									<span>Действителен ещё</span>
+									<span class="coupon-time" >
+										<strong>1</strong> день
+									</span>
+								</div>
 							</div>
 						</div>
 					</li>
@@ -149,7 +177,7 @@
 					<li class="promo-item">
 						<div class="promo-item-block">
 							<div  class="promo-item-block-left" >
-								<img alt="" src="/images/coupon-image.jpg" class="promo-item-img" >
+								<img alt="" src="/images/coupon-image-2.jpg" class="promo-item-img" >
 								<div class="vote-block" ></div>
 							</div>
 							<div  class="promo-item-block-center" >
@@ -159,14 +187,20 @@
 								<p>Без ввода промо-кода. Предложение ограничено. 
 								Скидка распространяется только на товары из данного раздела.</p>
 								
-								<a href="#">Использовать промокод</a>
-								<a href="#">все промокоды</a>
+								<a class="use-promo" href="#">Использовать промокод</a>
+								<a class="see-all-coupons" href="#">все промокоды</a>
 							</div>
 							<div  class="promo-item-block-right" >
-								<span>Действителен ещё</span>
-								<span class="coupon-time" >
-									<strong>1</strong> день
-								</span>
+								<div class="promo-item-sale-marker persent" >
+									<span>скидка</span>
+									<strong>50%</strong>
+								</div>
+								<div class="promo-item-coupon-timer" >
+									<span>Действителен ещё</span>
+									<span class="coupon-time" >
+										<strong>1</strong> день
+									</span>
+								</div>
 							</div>
 						</div>
 					</li>
@@ -174,7 +208,7 @@
 					<li class="promo-item">
 						<div class="promo-item-block">
 							<div  class="promo-item-block-left" >
-								<img alt="" src="/images/coupon-image.jpg" class="promo-item-img" >
+								<img alt="" src="/images/coupon-image-3.jpg" class="promo-item-img" >
 								<div class="vote-block" ></div>
 							</div>
 							<div  class="promo-item-block-center" >
@@ -184,14 +218,20 @@
 								<p>Без ввода промо-кода. Предложение ограничено. 
 								Скидка распространяется только на товары из данного раздела.</p>
 								
-								<a href="#">Использовать промокод</a>
-								<a href="#">все промокоды</a>
+								<a class="use-promo" href="#">Использовать промокод</a>
+								<a class="see-all-coupons" href="#">все промокоды</a>
 							</div>
 							<div  class="promo-item-block-right" >
-								<span>Действителен ещё</span>
-								<span class="coupon-time" >
-									<strong>1</strong> день
-								</span>
+								<div class="promo-item-sale-marker persent" >
+									<span>скидка</span>
+									<strong>50%</strong>
+								</div>
+								<div class="promo-item-coupon-timer" >
+									<span>Действителен ещё</span>
+									<span class="coupon-time" >
+										<strong>1</strong> день
+									</span>
+								</div>
 							</div>
 						</div>
 					</li>
@@ -222,6 +262,7 @@
 						<li><a href="#" ><img alt="" src="/images/shops.jpg" ></a></li>
 						<li><a href="#" ><img alt="" src="/images/shops.jpg" ></a></li>
 					</ul>
+					<a href="#" >Все магазины</a>
 				</div>
 				
 				<div class="content-sidebur-top-shops" >
@@ -270,29 +311,34 @@
 			Suspendisse potenti. Nulla imperdiet mollis tristique. Quisque sed leo venenatis, finibus sem lacinia, facilisis ipsum. 
 			Etiam sollicitudin, risus a placerat placerat, erat felis imperdiet dolor, non faucibus felis urna eu augue.</p> 
 		</article>
-		<footer>
-			<div class="footer-menu">
-				<ul>
-					<li><a href="#" >Популярные</a></li>
-					<li><a href="#" >Купоны по категориям</a></li>
-					<li><a href="#" >Новые</a></li>
-					<li><a href="#" >Горящие</a></li>
-					<li><a href="#" >Все магазины</a></li>
-				</ul>
-			</div>
-			<div class="copyright" >
-				<a href="#" >&copy; Умные-покупки.рф - агрегатор скидок и промокодов.</a>
-			</div>
-			<div class="social-block" >
-				<ul>
-					<li class="social-block-item fb" ><a href="#">facebook</a></li>
-					<li class="social-block-item vk"><a href="#">vk</a></li>
-					<li class="social-block-item tw"><a href="#">twitter</a></li>
-				</ul>
-			</div>
-			
-		</footer>
 	</div>
+	
+	<div class="footer-container" >
+		<div class="center-container" >
+			<footer>
+				<div class="footer-menu">
+					<ul>
+						<li><a href="#" >Популярные</a></li>
+						<li><a href="#" >Купоны по категориям</a></li>
+						<li><a href="#" >Новые</a></li>
+						<li><a href="#" >Горящие</a></li>
+						<li><a href="#" >Все магазины</a></li>
+					</ul>
+				</div>
+				<div class="copyright" >
+					<a href="#" >&copy; Умные-покупки.рф - агрегатор скидок и промокодов.</a>
+				</div>
+				<div class="social-block" >
+					<ul>
+						<li class="social-block-item fb" ><a href="#">facebook</a></li>
+						<li class="social-block-item vk"><a href="#">vkontakte</a></li>
+						<li class="social-block-item tw"><a href="#">twitter</a></li>
+					</ul>
+				</div>
+			</footer>
+		</div>
+	</div>
+
 	
 	<script type="text/javascript" href="/js/jquery-1.11.3.min.js" ></script>
 	<script type="text/javascript" href="/js/init.js" ></script>
